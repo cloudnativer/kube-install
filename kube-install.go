e9a888b1b1d477affa8b1b9a3a3b8e9118cbfdd3
cloudnativer <root@localhost.localdomain>
2021-05-10 09:22:20 +0800

    Remove the HA component dependency of the k8s master, and use the built-in IPVS to solve the LB problem.

--- kube-install.go
+++ kube-install.go
@@ -43,124 +43,124 @@ func main() {
 
       //Execute init command
       case opt == "init" :
-        fmt.Println("\nInitialization in progress, please wait...\n") 
-        time.Sleep(1 * time.Second)
-        for i := 1; i <= 100; i = i + 1 {
-          fmt.Fprintf(os.Stdout, "%d%% [%s]\r",i,kilib.ProgressBar(i,"#") + kilib.ProgressBar(100-i," "))
-          time.Sleep(time.Second * 1)
+          fmt.Println("\nInitialization in progress, please wait...\n") 
+          time.Sleep(1 * time.Second)
+          for i := 1; i <= 100; i = i + 1 {
+              fmt.Fprintf(os.Stdout, "%d%% [%s]\r",i,kilib.ProgressBar(i,"#") + kilib.ProgressBar(100-i," "))
+              time.Sleep(time.Second * 1)
         } 
         kilib.ShellExecute(currentdir+"/workflow/sshops-init.sh \""+softdir+"\" \""+currentdir+"\"")
         fmt.Println("\n\nInitialization completed!\n") 
 
       //Execute install command
       case opt == "install" :
-        fmt.Println("\nDeploying kubernetes cluster, please wait...\n") 
-        kilib.CheckParam(opt,"master",master)
-        kilib.CheckParam(opt,"node",node)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(currentdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+" "+node_str+"\" \""+softdir+"\" \""+currentdir+"\" \"install\"")
-        kilib.GeneralConfig(master_array, node_array, currentdir, softdir)
-        _, err_install := kilib.CopyFile(currentdir+"/workflow/general.inventory", currentdir+"/workflow/install.inventory")
-        kilib.CheckErr(err_install)
-        kilib.InstallConfig(master_array, node_array, currentdir, softdir)
-        kilib.InstallGenfile(currentdir)
-        if len(master_array) == 1{
-          kilib.OnemasterinstallYML(currentdir)
-          kilib.ShellExecute("ansible-playbook -i "+currentdir+"/workflow/install.inventory "+currentdir+"/workflow/k8scluster-onemasterinstall.yml")
-        }else{
-          kilib.InstallYML(currentdir)
-          kilib.ShellExecute("ansible-playbook -i "+currentdir+"/workflow/install.inventory "+currentdir+"/workflow/k8scluster-install.yml")
-        }
-        fmt.Println("Kubernetes cluster deployment operation execution completed!")
+          fmt.Println("\nDeploying kubernetes cluster, please wait...\n") 
+          kilib.CheckParam(opt,"master",master)
+          kilib.CheckParam(opt,"node",node)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(currentdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+" "+node_str+"\" \""+softdir+"\" \""+currentdir+"\" \"install\"")
+          kilib.GeneralConfig(master_array, node_array, currentdir, softdir)
+          _, err_install := kilib.CopyFile(currentdir+"/workflow/general.inventory", currentdir+"/workflow/install.inventory")
+          kilib.CheckErr(err_install)
+          kilib.InstallConfig(master_array, node_array, currentdir, softdir)
+          kilib.InstallGenfile(currentdir)
+          if len(master_array) == 1{
+              kilib.OnemasterinstallYML(currentdir)
+              kilib.ShellExecute("ansible-playbook -i "+currentdir+"/workflow/install.inventory "+currentdir+"/workflow/k8scluster-onemasterinstall.yml")
+          }else{
+              kilib.InstallYML(currentdir)
+              kilib.ShellExecute("ansible-playbook -i "+currentdir+"/workflow/install.inventory "+currentdir+"/workflow/k8scluster-install.yml")
+          }
+          fmt.Println("Kubernetes cluster deployment operation execution completed!")
 
       //Execute addnode command
       case opt == "addnode" :
-        fmt.Println("\nAdding k8s-node, please wait...\n") 
-        kilib.CheckParam(opt,"node",node)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+node_str+"\" \""+softdir+"\" \""+softdir+"\" \"addnode\"")
-        _, err_addnode := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/addnode.inventory")
-        kilib.CheckErr(err_addnode)
-        kilib.AddnodeConfig(node_array, softdir)
-        kilib.AddnodeYML(softdir)
-        kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/addnode.inventory "+softdir+"/workflow/k8scluster-addnode.yml")
-        fmt.Println("K8s-node add operation execution completed!")
+          fmt.Println("\nAdding k8s-node, please wait...\n") 
+          kilib.CheckParam(opt,"node",node)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+node_str+"\" \""+softdir+"\" \""+softdir+"\" \"addnode\"")
+          _, err_addnode := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/addnode.inventory")
+          kilib.CheckErr(err_addnode)
+          kilib.AddnodeConfig(node_array, softdir)
+          kilib.AddnodeYML(softdir)
+          kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/addnode.inventory "+softdir+"/workflow/k8scluster-addnode.yml")
+          fmt.Println("K8s-node add operation execution completed!")
 
       //Execute delnode command
       case opt == "delnode" :
-        fmt.Println("\nDeleting k8s-node, please wait...\n") 
-        kilib.CheckParam(opt,"node",node)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+node_str+"\" \""+softdir+"\" \""+softdir+"\" \"delnode\"")
-        _, err_delnode := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/delnode.inventory")
-        kilib.CheckErr(err_delnode)
-        kilib.DelnodeConfig(node_array, softdir)
-        kilib.DelnodeYML(softdir)
-        delnodeiplist := "{"+node+"}"
-        if len(node_array) == 1 { delnodeiplist = node }
-        kilib.ShellExecute("kubectl delete node "+delnodeiplist )
-        kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/delnode.inventory "+softdir+"/workflow/k8scluster-delnode.yml")
-        fmt.Println("K8s-node delete operation execution completed!")
+          fmt.Println("\nDeleting k8s-node, please wait...\n") 
+          kilib.CheckParam(opt,"node",node)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+node_str+"\" \""+softdir+"\" \""+softdir+"\" \"delnode\"")
+          _, err_delnode := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/delnode.inventory")
+          kilib.CheckErr(err_delnode)
+          kilib.DelnodeConfig(node_array, softdir)
+          kilib.DelnodeYML(softdir)
+          delnodeiplist := "{"+node+"}"
+          if len(node_array) == 1 { delnodeiplist = node }
+          kilib.ShellExecute("kubectl delete node "+delnodeiplist )
+          kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/delnode.inventory "+softdir+"/workflow/k8scluster-delnode.yml")
+          fmt.Println("K8s-node delete operation execution completed!")
 
       //Execute rebuildmaster command
       case opt == "rebuildmaster" :
-        fmt.Println("\nRebuilding k8s-master, please wait...\n")
-        kilib.CheckParam(opt,"master",master)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+"\" \""+softdir+"\" \""+softdir+"\" \"rebuildmaster\"")
-        _, err_rebuildmaster := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/rebuildmaster.inventory")
-        kilib.CheckErr(err_rebuildmaster)
-        kilib.RebuildmasterConfig(master_array, softdir)
-        kilib.InstallGenfile(softdir)
-        kilib.RebuildmasterYML(softdir)
-        kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/rebuildmaster.inventory "+softdir+"/workflow/k8scluster-rebuildmaster.yml")
-        fmt.Println("K8s-master rebuilt operation execution completed!")
+          fmt.Println("\nRebuilding k8s-master, please wait...\n")
+          kilib.CheckParam(opt,"master",master)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+"\" \""+softdir+"\" \""+softdir+"\" \"rebuildmaster\"")
+          _, err_rebuildmaster := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/rebuildmaster.inventory")
+          kilib.CheckErr(err_rebuildmaster)
+          kilib.RebuildmasterConfig(master_array, softdir)
+          kilib.InstallGenfile(softdir)
+          kilib.RebuildmasterYML(softdir)
+          kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/rebuildmaster.inventory "+softdir+"/workflow/k8scluster-rebuildmaster.yml")
+          fmt.Println("K8s-master rebuilt operation execution completed!")
 
       //Execute delmaster command
       case opt == "delmaster" :
-        fmt.Println("\nDeleting k8s-master, please wait...\n")
-        kilib.CheckParam(opt,"master",master)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+"\" \""+softdir+"\" \""+softdir+"\" \"delmaster\"")
-        _, err_delmaster := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/delmaster.inventory")
-        kilib.CheckErr(err_delmaster)
-        kilib.DelmasterConfig(master_array, softdir)
-        kilib.DelmasterYML(softdir)
-        kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/delmaster.inventory "+softdir+"/workflow/k8scluster-delmaster.yml")
-        fmt.Println("K8s-master delete operation execution completed!")
+          fmt.Println("\nDeleting k8s-master, please wait...\n")
+          kilib.CheckParam(opt,"master",master)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(softdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \"127.0.0.1 "+master_str+"\" \""+softdir+"\" \""+softdir+"\" \"delmaster\"")
+          _, err_delmaster := kilib.CopyFile(softdir+"/workflow/general.inventory", softdir+"/workflow/delmaster.inventory")
+          kilib.CheckErr(err_delmaster)
+          kilib.DelmasterConfig(master_array, softdir)
+          kilib.DelmasterYML(softdir)
+          kilib.ShellExecute("ansible-playbook -i "+softdir+"/workflow/delmaster.inventory "+softdir+"/workflow/k8scluster-delmaster.yml")
+          fmt.Println("K8s-master delete operation execution completed!")
 
       //Execute uninstall command
       case opt == "uninstall" :
-        fmt.Println("\nUninstalling kubernetes cluster, please wait...\n\n")
-        kilib.CheckParam(opt,"master",master)
-        kilib.CheckParam(opt,"node",node)
-        kilib.CheckParam(opt,"sshpwd",sshpwd)
-        kilib.ShellExecute(currentdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \""+master_str+" "+node_str+"\" \""+softdir+"\" \""+currentdir+"\" \"install\"")
-        //Create tmp workflow dir
-        err_rmdir:= os.RemoveAll("/tmp/workflow/")
-        kilib.CheckErr(err_rmdir)
-        err_mkdir := os.Mkdir("/tmp/workflow", 0666)
-        kilib.CheckErr(err_mkdir)
-        //Create tmp config files
-        kilib.GeneralConfig(master_array, node_array, currentdir, softdir)
-        _, err_uninstall := kilib.CopyFile(currentdir+"/workflow/general.inventory", "/tmp/workflow/uninstall.inventory")
-        kilib.CheckErr(err_uninstall)
-        kilib.UninstallConfig(node_array, master_array, "/tmp")
-        kilib.UninstallYML(currentdir)
-        //Uninstall kubernetes cluster process now
-        delnodeiplist := "{"+node+"}"
-        if len(node_array) == 1 { delnodeiplist = node }
-        fmt.Println("K8s-node list: "+delnodeiplist+" \n")
-        kilib.ShellExecute("kubectl delete node "+delnodeiplist+">/dev/null 2>&1")
-        fmt.Println("k8s-node delete operation execution completed!\n\nUninstall k8s-master and k8s-node software, please wait...")
-        kilib.ShellExecute("ansible-playbook -i /tmp/workflow/uninstall.inventory /tmp/workflow/k8scluster-uninstall.yml")
-        fmt.Println("K8s-master and k8s-node software uninstall operation execution completed!\n\n**********************************************************************************\n\n")
-        os.RemoveAll("/tmp/workflow/")
-        fmt.Println("Kubernetes cluster uninstall operation execution completed!\n\n")
+          fmt.Println("\nUninstalling kubernetes cluster, please wait...\n\n")
+          kilib.CheckParam(opt,"master",master)
+          kilib.CheckParam(opt,"node",node)
+          kilib.CheckParam(opt,"sshpwd",sshpwd)
+          kilib.ShellExecute(currentdir+"/workflow/sshkey-init.sh \""+sshpwd+"\" \""+master_str+" "+node_str+"\" \""+softdir+"\" \""+currentdir+"\" \"install\"")
+          //Create tmp workflow dir
+          err_rmdir:= os.RemoveAll("/tmp/workflow/")
+          kilib.CheckErr(err_rmdir)
+          err_mkdir := os.Mkdir("/tmp/workflow", 0666)
+          kilib.CheckErr(err_mkdir)
+          //Create tmp config files
+          kilib.GeneralConfig(master_array, node_array, currentdir, softdir)
+          _, err_uninstall := kilib.CopyFile(currentdir+"/workflow/general.inventory", "/tmp/workflow/uninstall.inventory")
+          kilib.CheckErr(err_uninstall)
+          kilib.UninstallConfig(node_array, master_array, "/tmp")
+          kilib.UninstallYML(currentdir)
+          //Uninstall kubernetes cluster process now
+          delnodeiplist := "{"+node+"}"
+          if len(node_array) == 1 { delnodeiplist = node }
+          fmt.Println("K8s-node list: "+delnodeiplist+" \n")
+          kilib.ShellExecute("kubectl delete node "+delnodeiplist+">/dev/null 2>&1")
+          fmt.Println("k8s-node delete operation execution completed!\n\nUninstall k8s-master and k8s-node software, please wait...")
+          kilib.ShellExecute("ansible-playbook -i /tmp/workflow/uninstall.inventory /tmp/workflow/k8scluster-uninstall.yml")
+          fmt.Println("K8s-master and k8s-node software uninstall operation execution completed!\n\n**********************************************************************************\n\n")
+          os.RemoveAll("/tmp/workflow/")
+          fmt.Println("Kubernetes cluster uninstall operation execution completed!\n\n")
 
       //Default output help information
       default:
-        kilib.ShowHelp()
+          kilib.ShowHelp()
 
     }
 
--- lib/wfcfg.go
+++ lib/wfcfg.go
@@ -21,30 +21,30 @@ func GeneralConfig(master_array []string, node_array []string, currentdir string
     master_num := len(master_array)
     node_num := len(node_array)
     for i := 0; i < master_num; i++ {
-      if i > 0{
-        master_iplist = master_iplist + ","
-        etcd_initial = etcd_initial + ","
-        etcd_endpoints = etcd_endpoints + ","
-      }
-      master_iplist = master_iplist+"\\\""+master_array[i]+"\\\""
-      etcd_initial = etcd_initial+"kube"+strconv.Itoa(i)+"=https://"+master_array[i]+":2380"
-      etcd_endpoints = etcd_endpoints+"https://"+master_array[i]+":2379"
-      master_ipvsinit = master_ipvsinit+" && ipvsadm -a -t 10.254.0.1:443 -r "+master_array[i]+":6443 -m"
+        if i > 0{
+            master_iplist = master_iplist + ","
+            etcd_initial = etcd_initial + ","
+            etcd_endpoints = etcd_endpoints + ","
+        }
+        master_iplist = master_iplist+"\\\""+master_array[i]+"\\\""
+        etcd_initial = etcd_initial+"kube"+strconv.Itoa(i)+"=https://"+master_array[i]+":2380"
+        etcd_endpoints = etcd_endpoints+"https://"+master_array[i]+":2379"
+        master_ipvsinit = master_ipvsinit+" && ipvsadm -a -t 10.254.0.1:443 -r "+master_array[i]+":6443 -m"
     }
     for i := 0; i < node_num; i++ {
-      ingress_upstream = ingress_upstream+"server "+node_array[i]+":80 max_fails=3 fail_timeout=30s;"
+        ingress_upstream = ingress_upstream+"server "+node_array[i]+":80 max_fails=3 fail_timeout=30s;"
     }
     inventory_file.WriteString("master_iplist=\""+master_iplist+"\"\netcd_initial=\""+etcd_initial+"\"\netcd_endpoints=\""+etcd_endpoints+"\"\ningress_upstream=\""+ingress_upstream+"\"\nmaster_ipvsinit = \""+master_ipvsinit+"\"\nmaster_vip=\"10.254.0.1\"\nmaster_vport=\"443\"\n")
     //Setting the scheduling IP for addons
     switch {
-      case node_num == 1 :
-        inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[0]+"\"\naddons_ip3=\""+node_array[0]+"\"\n")
-      case node_num == 2 :
-        inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[1]+"\"\naddons_ip3=\""+node_array[1]+"\"\n")
-      case node_num >= 3 :
-        inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[1]+"\"\naddons_ip3=\""+node_array[2]+"\"\n")
-      default:
-        panic("You must install at least one k8s-node to ensure that the cluster is running properly!")
+        case node_num == 1 :
+            inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[0]+"\"\naddons_ip3=\""+node_array[0]+"\"\n")
+        case node_num == 2 :
+            inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[1]+"\"\naddons_ip3=\""+node_array[1]+"\"\n")
+        case node_num >= 3 :
+            inventory_file.WriteString("\n### addons_ip configuration ###\naddons_ip1=\""+node_array[0]+"\"\naddons_ip2=\""+node_array[1]+"\"\naddons_ip3=\""+node_array[2]+"\"\n")
+        default:
+            panic("You must install at least one k8s-node to ensure that the cluster is running properly!")
     }
     inventory_file.WriteString("\n### traefik configuration ###\ntraefik_admin_port=\"80\"\ntraefik_data_port=\"8080\"\n")
     inventory_file.WriteString("\n### k8s-network configuration ###\nservice_cidr=\"10.254.0.0/16\"\nservice_svc_ip=\"10.254.0.1\"\nservice_dns_svc_ip=\"10.254.0.2\"\npod_cidr=\"172.30.0.0/16\"\n\n\n")
@@ -61,19 +61,19 @@ func InstallConfig(master_array []string, node_array []string, currentdir string
     write.WriteString("\n[master]\n")
     master_num := len(master_array)
     for i := 0; i < master_num; i++ {
-      CheckIP(master_array[i])
-      write.WriteString(master_array[i]+" ip="+master_array[i]+"\n")
+        CheckIP(master_array[i])
+        write.WriteString(master_array[i]+" ip="+master_array[i]+"\n")
     }
     write.WriteString("\n[etcd]\n")
     for i := 0; i < master_num; i++ {
-      write.WriteString(master_array[i]+" ip="+master_array[i]+" etcdname=kube"+strconv.Itoa(i)+"\n")
+        write.WriteString(master_array[i]+" ip="+master_array[i]+" etcdname=kube"+strconv.Itoa(i)+"\n")
     }
     //Generate node configuration
     write.WriteString("\n\n\n###-----------------------------------k8s-node host list-------------------------------###\n")
     write.WriteString("\n[node]\n")
     for i := 0; i < len(node_array); i++ {
-      CheckIP(node_array[i])
-      write.WriteString(node_array[i]+" ip="+node_array[i]+"\n")
+        CheckIP(node_array[i])
+        write.WriteString(node_array[i]+" ip="+node_array[i]+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"master\n"+"etcd\n"+"node\n"+"\n\n")
     write.Flush()
@@ -89,8 +89,8 @@ func AddnodeConfig(addnode_array []string, softdir string) {
     write.WriteString("###---------------------------------New k8s-node host list------------------------------###\n")
     write.WriteString("\n[addnode]\n")
     for i := 0; i < len(addnode_array); i++ {
-      CheckIP(addnode_array[i])
-      write.WriteString(addnode_array[i]+" ip="+addnode_array[i]+"\n")
+        CheckIP(addnode_array[i])
+        write.WriteString(addnode_array[i]+" ip="+addnode_array[i]+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"addnode\n\n\n")
     write.Flush()
@@ -106,8 +106,8 @@ func DelnodeConfig(delnode_array []string, softdir string) {
     write.WriteString("###---------------------------------To delete k8s-node host list------------------------------###\n")
     write.WriteString("\n[delnode]\n")
     for i := 0; i < len(delnode_array); i++ {
-      CheckIP(delnode_array[i])
-      write.WriteString(delnode_array[i]+" ip="+delnode_array[i]+"\n")
+        CheckIP(delnode_array[i])
+        write.WriteString(delnode_array[i]+" ip="+delnode_array[i]+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"delnode\n\n\n")
     write.Flush()
@@ -131,13 +131,13 @@ func RebuildmasterConfig(rebuildmaster_array []string, softdir string) {
     write.WriteString("\n[master]\n")
     rebuildmaster_num := len(rebuildmaster_array)
     for i := 0; i < rebuildmaster_num; i++ {
-      CheckIP(rebuildmaster_array[i])
-      write.WriteString(rebuildmaster_array[i]+" ip="+rebuildmaster_array[i]+"\n")
+        CheckIP(rebuildmaster_array[i])
+        write.WriteString(rebuildmaster_array[i]+" ip="+rebuildmaster_array[i]+"\n")
     }
     write.WriteString("\n[etcd]\n")
     for i := 0; i < rebuildmaster_num; i++ {
-      etcdname := ShellOutput("cat "+softdir+"/workflow/install.inventory  | grep etcdname | grep "+rebuildmaster_array[i]+" | cut -d \"=\" -f 3,4 | head -1")
-      write.WriteString(rebuildmaster_array[i]+" ip="+rebuildmaster_array[i]+" etcdname="+etcdname+"\n")
+        etcdname := ShellOutput("cat "+softdir+"/workflow/install.inventory  | grep etcdname | grep "+rebuildmaster_array[i]+" | cut -d \"=\" -f 3,4 | head -1")
+        write.WriteString(rebuildmaster_array[i]+" ip="+rebuildmaster_array[i]+" etcdname="+etcdname+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"master\n"+"etcd\n"+"\n\n")
     write.Flush()
@@ -153,8 +153,8 @@ func DelmasterConfig(delmaster_array []string, softdir string) {
     write.WriteString("###---------------------------------To delete k8s-master host list------------------------------###\n")
     write.WriteString("\n[delmaster]\n")
     for i := 0; i < len(delmaster_array); i++ {
-      CheckIP(delmaster_array[i])
-      write.WriteString(delmaster_array[i]+" ip="+delmaster_array[i]+"\n")
+        CheckIP(delmaster_array[i])
+        write.WriteString(delmaster_array[i]+" ip="+delmaster_array[i]+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"delmaster\n\n\n")
     write.Flush()
@@ -170,14 +170,14 @@ func UninstallConfig(delnode_array []string, delmaster_array []string, softdir s
     write.WriteString("###---------------------------------To delete k8s-node host list------------------------------###\n")
     write.WriteString("\n[delnode]\n")
     for i := 0; i < len(delnode_array); i++ {
-      CheckIP(delnode_array[i])
-      write.WriteString(delnode_array[i]+" ip="+delnode_array[i]+"\n")
+        CheckIP(delnode_array[i])
+        write.WriteString(delnode_array[i]+" ip="+delnode_array[i]+"\n")
     }
     write.WriteString("###---------------------------------To delete k8s-master host list------------------------------###\n")
     write.WriteString("\n[delmaster]\n")
     for i := 0; i < len(delmaster_array); i++ {
-      CheckIP(delmaster_array[i])
-      write.WriteString(delmaster_array[i]+" ip="+delmaster_array[i]+"\n")
+        CheckIP(delmaster_array[i])
+        write.WriteString(delmaster_array[i]+" ip="+delmaster_array[i]+"\n")
     }
     write.WriteString("\n[k8s:children]\n"+"master1\n"+"delmaster\n"+"delnode\n\n\n")
     write.Flush()
